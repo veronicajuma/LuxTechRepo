01769750b6d2ddf8f37066a6179d30c3e1f7147b
veronicajuma <63641245+veronicajuma@users.noreply.github.com>
2020-10-03 00:05:29 +0300

    Add files via upload

---
+++ precache-manifest.ccb4b0e5bb6b02af185850e9ea887992.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b058a95491b058f3ed3",
+    "url": "/static/css/main.66276d43.chunk.css"
+  },
+  {
+    "revision": "7b058a95491b058f3ed3",
+    "url": "/static/js/main.20742946.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a9b6a45010783c83c85b",
+    "url": "/static/js/2.2e9abb2f.chunk.js"
+  },
+  {
+    "revision": "99cd7023018b0c2312d818f5f881ffd4",
+    "url": "/index.html"
+  }
+];
